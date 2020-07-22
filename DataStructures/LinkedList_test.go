28248a524f70c560b9a779f286f84a80a8e42c66
Rick Matusiewicz <3045140+rmatusiewicz@users.noreply.github.com>
2020-07-22 12:30:40 -0700

    LinkedList implementation

---
+++ DataStructures/LinkedList_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestInsert(t *testing.T) {
+	l := NewLinkedList()
+	l.insert(2)
+
+	if len(l.nodes) != 1 || l.head.Key != 2 {
+		t.Error("Should have 2 at head")
+	}
+}
+
+func TestDelete(t *testing.T) {
+	l := NewLinkedList()
+	l.insert(2)
+	l.insert(3)
+	l.insert(4)
+
+	l.delete(3)
+	if len(l.nodes) != 2 {
+		t.Error("Failed to delete element 3")
+	}
+
+	l.delete(4)
+
+	fmt.Println(l.head.Key)
+	fmt.Println(l.head.Next)
+	if l.head.Key != 2 || l.head.Next != nil {
+		t.Error("Failed to delete 3 or 4")
+	}
+}
+
+func TestReverse(t *testing.T) {
+	l := NewLinkedList()
+	l.insert(1)
+	l.insert(2)
+	l.insert(3)
+	l.insert(4)
+	if l.head.Key != 4 {
+		t.Error("Head should be at 4")
+	}
+
+	l.reverse()
+	l.reverse()
+
+	l.print()
+
+	if l.head.Key != 1 {
+		t.Error("Head should be at 1")
+	}
+
+}
